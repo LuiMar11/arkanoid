2f011b817fa556a0ef8e0367cc60397f28d33eca
Luisa <luisachalarca@gmail.com>
2024-03-06 10:50:56 -0500

    first commit

---
+++ js/audio.js
@@ -0,0 +1,33 @@
+// Rebote pared ─────────────────────────────────────────────────────────────
+function audioPared() {
+  var audio = new Audio('audio/rebotePared.mp3');
+  audio.play();
+}
+
+// Rebote jugador ─────────────────────────────────────────────────────────────
+function audioPlayer() {
+  var audio = new Audio('audio/rebotePlayer.mp3');
+  audio.play();
+}
+
+// Game Over ─────────────────────────────────────────────────────────────
+function audioGameOver() {
+  var audio = new Audio('audio/gameOver.mp3');
+  audio.play();
+}
+
+// Ladrillo ─────────────────────────────────────────────────────────────
+function audioLadrillo() {
+  var audio = new Audio('audio/ladrillo.mp3');
+  audio.play();
+}
+// Ladrillo ─────────────────────────────────────────────────────────────
+function audioNuevoNivel() {
+  var audio = new Audio('audio/nuevoNivel.mp3');
+  audio.play();
+}
+// Ladrillo ─────────────────────────────────────────────────────────────
+function audioMusica() {
+  var audio = new Audio('audio/musica.mp3');
+  audio.play();
+}
